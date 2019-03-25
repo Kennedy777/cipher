4578abe3e6aac8e9dd8934b3616cec69d27b24c4
Kevin Garvey and Marguerite Kennedy <kevin@>
2019-03-25 16:14:14 -0700

    started cipher script.js

---
+++ Desktop/cipher/js/scripts.js
@@ -0,0 +1,12 @@
+var sentence = prompt("Sentence here, no periods please.");
+var upperCase1 = sentence.charAt(0);
+var upperCase2 = sentence.charAt(sentence.length-1);
+function capitalize(mychar) {
+  let upperC = mychar.toUpperCase();
+  answer += upperC;
+var answer2 = answer.charAt(answer.length-1) + answer.charAt(0)
+let answer = "";
+capitalize(upperCase1);
+capitalize(upperCase2);
+alert(answer);
+};
